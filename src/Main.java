946c7ce33759176c0045adee268187a57c12ddd2
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2021-10-26 12:35:32 +0100

    Creación processBuilder en el que ejecuta varios procesos según el número que pases

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        
+        ProcessBuilder pB = new ProcessBuilder();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce número de ventanas");
+        int numVentanas = sc.nextInt();
+
+        pB.command("cmd.exe", "/c" ,"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe", "www.google.com");
+        try {
+            Process process = null;
+            for (int i = 0; i < numVentanas; i++) {
+                process = pB.start();
+            }
+            System.out.println(process.isAlive());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
